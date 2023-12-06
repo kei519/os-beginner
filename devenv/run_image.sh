f3090e87918e99a47b58e3c080c2a1aec6279008
kei519 <masaki.keigo.q00@kyoto-u.jp>
2023-12-07 02:59:34 +0900

    QEMU 起動オプションの変更

--- devenv/run_image.sh
+++ devenv/run_image.sh
@@ -16,5 +16,7 @@ fi
 qemu-system-x86_64 \
 	-drive if=pflash,format=raw,readonly=on,file=$DEVENV_DIR/OVMF_CODE.fd \
 	-drive if=pflash,format=raw,file=$DEVENV_DIR/OVMF_VARS.fd \
-	-monitor stdio \
-	-hda $DISK_IMG
+	-drive if=ide,index=0,media=disk,format=raw,file=$DISK_IMG \
+	-device nec-usb-xhci,id=xhci \
+	-device usb-mouse -device usb-kbd \
+	-monitor stdio
