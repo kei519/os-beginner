3edabb0849cd7e49bc20b9d069aedd5cda66b456
kei519 <masaki.keigo.q00@kyoto-u.jp>
2024-04-07 00:10:23 +0900

    RwLock の修正

--- mikan-os/kernel/src/sync.rs
+++ mikan-os/kernel/src/sync.rs
@@ -1,29 +1,27 @@
 use core::{
     cell::UnsafeCell,
+    hint::spin_loop,
+    mem::MaybeUninit,
     ops::{Deref, DerefMut},
-    sync::atomic::{AtomicBool, AtomicIsize, Ordering},
+    sync::atomic::{AtomicBool, AtomicIsize, Ordering::*},
 };
 
 /// [RwLock] 等が借用されていないときの `count` の値。
 const UNUSED: isize = 0;
-/// [RwLock] 等が不変参照される最大値。
+/// [RwLock] 等が共有参照される最大値。
 const BORROW_MAX: isize = isize::MAX / 2;
 
 /// スレッドセーフな内部可変性を持つ構造体。
+///
+/// 1度に1つの排他参照、または複数の共有参照を作れる。
 pub(crate) struct RwLock<T> {
     /// 内部データを持つ。
     data: UnsafeCell<T>,
     /// 参照カウンタ。
     counter: AtomicIsize,
-    /// 不変参照を作るときに参照カウンタをインクリメントするが、
-    /// このときに可変参照が作られていて、参照カウンタが0になってしまっていると、
-    /// 新しく可変参照が作れてしまう。
-    /// それを阻止するのに、不変参照作成時に参照カウンタをロックするのに使う。
-    locker: AtomicBool,
 }
 
-unsafe impl<T: Send> Sync for RwLock<T> {}
-unsafe impl<T: Send> Send for RwLock<T> {}
+unsafe impl<T: Send + Sync> Sync for RwLock<T> {}
 
 impl<T> RwLock<T> {
     /// [RwLock] のコンストラクタ。
@@ -31,61 +29,48 @@ impl<T> RwLock<T> {
         Self {
             data: UnsafeCell::new(value),
             counter: AtomicIsize::new(0),
-            locker: AtomicBool::new(false),
         }
     }
 
-    /// 参照カウンタのためのロックを得る。
-    ///
-    /// ロックを得るまで無限に待機する。
-    fn lock(&self) -> LockKey<'_> {
-        while let Err(_) =
-            self.locker
-                .compare_exchange(false, true, Ordering::Release, Ordering::Relaxed)
-        {}
-
-        LockKey {
-            locker: &self.locker,
-        }
-    }
-
-    /// 読み取りのための不変参照を取得する。
+    /// 読み取りのための共有参照を取得する。
     ///
     /// 参照が得られるまで無限に待機する。
-    pub(crate) fn read(&self) -> RwRead<'_, T> {
+    pub(crate) fn read(&self) -> ReadGuard<'_, T> {
+        let mut c = self.counter.load(Relaxed);
         loop {
-            let _locker = self.lock();
-            let prev_count = self.counter.fetch_add(1, Ordering::Release);
-            // 元々参照がない or 不変参照しかない状況で、
-            // 不変参照の数が `BORROW_MAX` 以下のとき、新たな参照を作って返す。
-            // それまでは待機する。
-            if prev_count >= UNUSED && prev_count <= BORROW_MAX {
-                break;
+            if c < UNUSED || c > BORROW_MAX {
+                spin_loop();
+                c = self.counter.load(Relaxed);
+                continue;
             }
-            self.counter.fetch_sub(1, Ordering::Release);
-        }
+            if let Err(e) = self
+                .counter
+                .compare_exchange_weak(c, c + 1, Acquire, Relaxed)
+            {
+                c = e;
+                continue;
+            }
 
-        RwRead {
-            data: unsafe { &*self.data.get() },
-            counter: &self.counter,
+            return ReadGuard {
+                data: unsafe { &*self.data.get() },
+                counter: &self.counter,
+            };
         }
     }
 
-    /// 書き込みのための可変参照を取得する。
+    /// 書き込みのための排他参照を取得する。
     ///
     /// 参照が得られるまで無限に待機する。
-    pub(crate) fn write(&self) -> RwWrite<'_, T> {
-        loop {
-            let prev_count = self.counter.fetch_sub(1, Ordering::Release);
-            // 元々参照が作られていない場合に、新たに可変参照を作って返す。
-            // それまでは待機する。
-            if prev_count == UNUSED {
-                break;
-            }
-            self.counter.fetch_add(1, Ordering::Release);
+    pub(crate) fn write(&self) -> WriteGuard<'_, T> {
+        while self
+            .counter
+            .compare_exchange(0, 1, Acquire, Relaxed)
+            .is_err()
+        {
+            spin_loop();
         }
 
-        RwWrite {
+        WriteGuard {
             data: unsafe { &mut *self.data.get() },
             counter: &self.counter,
         }
@@ -94,29 +79,25 @@ impl<T> RwLock<T> {
 
 /// 可変の static 変数として使える、実行時に初期化が可能な [RwLock]。
 ///
-/// ただし、初期化していないアクセスは `panic!` を引き起こすので注意。
+/// ただし、初期化していないアクセスは未定義動作を引き起こすので注意。
 pub(crate) struct OnceRwLock<T> {
     /// 内部データを持つ。
-    data: UnsafeCell<Option<T>>,
+    data: UnsafeCell<MaybeUninit<T>>,
     /// 参照カウンタ。
     counter: AtomicIsize,
-    /// 不変参照を作るときに参照カウンタをインクリメントするが、
-    /// このときに可変参照が作られていて、参照カウンタが0になってしまっていると、
-    /// 新しく可変参照が作れてしまう。
-    /// それを阻止するのに、不変参照作成時に参照カウンタをロックするのに使う。
-    locker: AtomicBool,
+    /// 初期化されているかどうかを表す。
+    is_initialized: AtomicBool,
 }
 
-unsafe impl<T: Send> Sync for OnceRwLock<T> {}
-unsafe impl<T: Send> Send for OnceRwLock<T> {}
+unsafe impl<T: Send + Sync> Sync for OnceRwLock<T> {}
 
 impl<T> OnceRwLock<T> {
     /// [OnceMutex] のコンストラクタ。
     pub(crate) const fn new() -> Self {
         Self {
-            data: UnsafeCell::new(None),
+            data: UnsafeCell::new(MaybeUninit::uninit()),
             counter: AtomicIsize::new(0),
-            locker: AtomicBool::new(false),
+            is_initialized: AtomicBool::new(false),
         }
     }
 
@@ -125,15 +106,14 @@ impl<T> OnceRwLock<T> {
     /// * `value` - 設定する値。
     pub(crate) const fn from_value(value: T) -> Self {
         Self {
-            data: UnsafeCell::new(Some(value)),
+            data: UnsafeCell::new(MaybeUninit::new(value)),
             counter: AtomicIsize::new(0),
-            locker: AtomicBool::new(false),
+            is_initialized: AtomicBool::new(true),
         }
     }
 
     pub(crate) fn is_initialized(&self) -> bool {
-        let _locker = self.lock();
-        unsafe { &*self.data.get() }.is_some()
+        self.is_initialized.load(Relaxed)
     }
 
     /// [OnceMutex] の初期化を行う。
@@ -145,126 +125,106 @@ impl<T> OnceRwLock<T> {
     ///
     /// 既に初期化されている場合は初期化されない。
     pub(crate) fn init(&self, value: T) -> bool {
-        let _locker = self.lock();
-        while let Err(_) =
-            self.counter
-                .compare_exchange(0, -1, Ordering::Release, Ordering::Relaxed)
-        {}
-
-        let data = self.data.get();
-        match unsafe { &*data } {
-            Some(_) => {
-                self.counter.fetch_add(1, Ordering::Release);
-                false
-            }
-            None => {
-                unsafe { *data = Some(value) };
-                self.counter.fetch_add(1, Ordering::Release);
-                true
-            }
+        while self
+            .counter
+            .compare_exchange(0, -1, Acquire, Relaxed)
+            .is_err()
+        {
+            spin_loop();
         }
+
+        let ret = if self.is_initialized() {
+            false
+        } else {
+            unsafe { (*self.data.get()).write(value) };
+            self.is_initialized.store(true, Relaxed);
+            true
+        };
+
+        self.counter.store(0, Release);
+        ret
     }
 
-    /// 参照カウンタのためのロックを得る。
-    ///
-    /// ロックを得るまで無限に待機する。
-    fn lock(&self) -> LockKey<'_> {
-        while let Err(_) =
-            self.locker
-                .compare_exchange(false, true, Ordering::Release, Ordering::Relaxed)
-        {}
-
-        LockKey {
-            locker: &self.locker,
-        }
-    }
-
-    /// 読み取りのための不変参照を取得する。
+    /// 読み取りのための共有参照を取得する。
     ///
     /// 参照が得られるまで無限に待機する。
-    pub(crate) fn read(&self) -> RwRead<'_, T> {
-        self.read_checked()
-            .unwrap_or_else(|| panic!("uninitialized value was accessed."))
-    }
-
-    /// 読み取りのための不変参照を取得する。
     ///
-    /// [read()][Self::read()] との違いは、初期化が行われていない場合に `panic!` でなく、
-    /// `None` が返ること。
-    pub(crate) fn read_checked(&self) -> Option<RwRead<'_, T>> {
+    /// ただし、初期化されていない場合の動作は未定義。
+    pub(crate) fn read(&self) -> ReadGuard<'_, T> {
+        let mut c = self.counter.load(Relaxed);
         loop {
-            let _locker = self.lock();
-            let prev_count = self.counter.fetch_add(1, Ordering::Release);
-            // 元々参照がない or 不変参照しかない状況で、
-            // 不変参照の数が `BORROW_MAX` 以下のとき、新たな参照を作って返す。
-            // それまでは待機する。
-            if prev_count >= UNUSED && prev_count <= BORROW_MAX {
-                break;
+            if c < UNUSED || c > BORROW_MAX {
+                spin_loop();
+                c = self.counter.load(Relaxed);
+                continue;
             }
-            self.counter.fetch_sub(1, Ordering::Release);
-        }
+            if let Err(e) = self
+                .counter
+                .compare_exchange_weak(c, c + 1, Acquire, Relaxed)
+            {
+                c = e;
+                continue;
+            }
 
-        match unsafe { (*self.data.get()).as_ref() } {
-            Some(data) => Some(RwRead {
-                data: data,
+            return ReadGuard {
+                data: unsafe { (*self.data.get()).assume_init_ref() },
                 counter: &self.counter,
-            }),
-            None => None,
+            };
         }
     }
 
-    /// 書き込みのための可変参照を取得する。
+    /// 読み取りのための共有参照を取得する。
+    ///
+    /// [read()][Self::read()] との違いは、初期化が行われていない場合に未定義動作でなく、
+    /// `None` が返ること。
+    pub(crate) fn read_checked(&self) -> Option<ReadGuard<'_, T>> {
+        if self.is_initialized() {
+            Some(self.read())
+        } else {
+            None
+        }
+    }
+
+    /// 書き込みのための排他参照を取得する。
     ///
     /// 参照が得られるまで無限に待機する。
-    pub(crate) fn write(&self) -> RwWrite<'_, T> {
-        self.write_checked()
-            .unwrap_or_else(|| panic!("uninitialized value has accessd"))
-    }
-
-    /// 書き込みのための可変参照を取得する。
     ///
-    /// [write()][Self::write()] との違いは、初期化が行われていない場合に `panic!` でなく、
-    /// `None` が返ること。
-    pub(crate) fn write_checked(&self) -> Option<RwWrite<'_, T>> {
-        loop {
-            let _locker = self.lock();
-            let prev_count = self.counter.fetch_sub(1, Ordering::Release);
-            // 元々参照が作られていない場合に、新たに可変参照を作って返す。
-            // それまでは待機する。
-            if prev_count == UNUSED {
-                break;
-            }
-            self.counter.fetch_add(1, Ordering::Release);
+    /// ただし、初期化されていない場合の動作は未定義。
+    pub(crate) fn write(&self) -> WriteGuard<'_, T> {
+        while self
+            .counter
+            .compare_exchange(UNUSED, -1, Acquire, Relaxed)
+            .is_err()
+        {
+            spin_loop();
         }
 
-        match unsafe { (*self.data.get()).as_mut() } {
-            Some(data) => Some(RwWrite {
-                data: data,
-                counter: &self.counter,
-            }),
-            None => None,
+        WriteGuard {
+            data: unsafe { (*self.data.get()).assume_init_mut() },
+            counter: &self.counter,
         }
     }
-}
 
-/// [OnceMutex] の `count` の操作をロック、ロック解除するときに使用される構造体。
-struct LockKey<'this> {
-    locker: &'this AtomicBool,
-}
-
-impl<'this> Drop for LockKey<'this> {
-    fn drop(&mut self) {
-        self.locker.store(false, Ordering::Release);
+    /// 書き込みのための排他参照を取得する。
+    ///
+    /// [write()][Self::write()] との違いは、初期化が行われていない場合に未定義動作でなく、
+    /// `None` が返ること。
+    pub(crate) fn write_checked(&self) -> Option<WriteGuard<'_, T>> {
+        if self.is_initialized() {
+            Some(self.write())
+        } else {
+            None
+        }
     }
 }
 
 /// [RwLock] を読み取るときに、そのデータと参照カウンタを管理するために使われる構造体。
-pub(crate) struct RwRead<'this, T> {
+pub(crate) struct ReadGuard<'this, T> {
     data: &'this T,
     counter: &'this AtomicIsize,
 }
 
-impl<'this, T> Deref for RwRead<'this, T> {
+impl<T> Deref for ReadGuard<'_, T> {
     type Target = T;
 
     fn deref(&self) -> &Self::Target {
@@ -272,19 +232,19 @@ impl<'this, T> Deref for RwRead<'this, T> {
     }
 }
 
-impl<'this, T> Drop for RwRead<'this, T> {
+impl<T> Drop for ReadGuard<'_, T> {
     fn drop(&mut self) {
-        self.counter.fetch_sub(1, Ordering::Release);
+        self.counter.fetch_sub(1, Release);
     }
 }
 
 /// [RwLock] に書き込むときに、そのデータと参照カウンタを管理するために使われる構造体。
-pub(crate) struct RwWrite<'this, T> {
+pub(crate) struct WriteGuard<'this, T> {
     data: &'this mut T,
     counter: &'this AtomicIsize,
 }
 
-impl<'this, T> Deref for RwWrite<'this, T> {
+impl<T> Deref for WriteGuard<'_, T> {
     type Target = T;
 
     fn deref(&self) -> &Self::Target {
@@ -292,14 +252,14 @@ impl<'this, T> Deref for RwWrite<'this, T> {
     }
 }
 
-impl<'this, T> DerefMut for RwWrite<'this, T> {
+impl<T> DerefMut for WriteGuard<'_, T> {
     fn deref_mut(&mut self) -> &mut Self::Target {
         self.data
     }
 }
 
-impl<'this, T> Drop for RwWrite<'this, T> {
+impl<T> Drop for WriteGuard<'_, T> {
     fn drop(&mut self) {
-        self.counter.fetch_add(1, Ordering::Release);
+        self.counter.store(0, Release);
     }
 }
