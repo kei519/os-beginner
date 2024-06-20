1d2a170b38f7873f9abb04914ffc5674614283c3
kei519 <masaki.keigo.q00@kyoto-u.jp>
2024-06-20 12:06:07 +0900

    アプリをビルドできるように

--- devenv/make_usb.sh
+++ devenv/make_usb.sh
@@ -26,5 +26,20 @@ sudo cp $EFI_FILE $MOUNT_POINT/EFI/BOOT/BOOTX64.EFI
 if [ "$ANOTHER_FILE" != "" ]; then
 	sudo cp $ANOTHER_FILE $MOUNT_POINT/
 fi
+
+if [ "$APPS_DIR" != "" ]; then
+	mkdir $MOUNT_POINT/$APPS_DIR
+fi
+
+for APP in $(ls "${MIKANOS_DIR}/apps"); do
+	if [ -f "$MIKANOS_DIR/apps/${APP}/${APP}" ]; then
+		cp "${MIKANOS_DIR}/apps/${APP}/${APP}" ${MOUNT_POINT}/${APPS_DIR}/
+	fi
+done
+
+if [ "${RESOURCE_DIR}" != "" ]; then
+	cp "${MIKANOS_DIR}/${RESOURCE_DIR}/*" "${MOUNT_POINT}"
+fi
+
 sleep 0.5
 sudo umount $MOUNT_POINT
