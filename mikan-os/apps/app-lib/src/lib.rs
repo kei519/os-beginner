d72dba0ed9857d93387d39cde81877459fc9eded
kei519 <masaki.keigo.q00@kyoto-u.jp>
2024-06-26 20:45:38 +0900

    app-lib: システムコール用のマクロと、既にあるシステムコールを宣言

--- mikan-os/apps/app-lib/src/lib.rs
+++ mikan-os/apps/app-lib/src/lib.rs
@@ -1,4 +1,7 @@
 #![no_std]
 #![cfg(target_arch = "x86_64")]
 
+mod syscall;
+
 pub mod buf;
+pub mod errno;
---
+++ mikan-os/apps/app-lib/src/syscall.rs
@@ -0,0 +1,42 @@
+/// __`$name` という名前で宣言する。
+/// 引数の型は全て [u64]。
+macro_rules! syscall {
+    ($name:ident, $number:expr, $($args:ident),*) => {
+        ::paste::paste! {
+            extern "sysv64" {
+                #[doc = concat!(
+                    "システムコール: ",
+                    stringify!($name),
+                    "（",
+                    stringify!($number),
+                    "）",
+                )]
+                pub(crate) fn [<__ $name>]($($args: u64),*) -> SysResult;
+            }
+
+            ::core::arch::global_asm! {
+                concat!{
+                    ".global ", stringify!([<__ $name>]), "\n",
+                    stringify!([<__ $name>]), ":\n",
+                    // EAX にシステムコール番号を入れる
+                    "    mov eax, ", $number,
+                        r#"
+    mov r10, rcx  # RCX に RIP が保存されるため、System-V ABI では R10 で渡す
+    syscall
+    ret
+                        "#,
+                }
+            }
+        }
+    };
+}
+
+syscall!(log_string, 0x8000_0000, log_level, s);
+syscall!(put_string, 0x8000_0001, fd, buf, len);
+
+#[repr(C)]
+#[derive(Debug, Default, Clone, Copy)]
+pub struct SysResult {
+    pub value: u64,
+    pub error: i32,
+}
