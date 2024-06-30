70c6c162b51fbd09be07ca21eaa0b9a1419ee79d
kei519 <masaki.keigo.q00@kyoto-u.jp>
2024-06-30 17:53:26 +0900

    stars: タイマー計測の追加

--- mikan-os/apps/stars/src/main.rs
+++ mikan-os/apps/stars/src/main.rs
@@ -3,7 +3,14 @@
 
 use core::{panic::PanicInfo, sync::atomic::Ordering};
 
-use app_lib::{args::Args, graphics, kernel_log, logger::LogLevel, main, ERRNO};
+use app_lib::{
+    args::Args,
+    graphics, kernel_log,
+    logger::LogLevel,
+    main, println,
+    time::{self, TimerInfo},
+    ERRNO,
+};
 use rand::{rngs::SmallRng, Rng, SeedableRng};
 
 extern crate app_lib;
@@ -22,12 +29,26 @@ fn main(args: Args) -> i32 {
         .get_as_str(1)
         .and_then(|arg| arg.parse().ok())
         .unwrap_or(100);
-    let mut rng = SmallRng::from_seed([0; 32]);
+
+    let TimerInfo {
+        tick: tick_start,
+        freq: timer_freq,
+    } = time::get_current_tick();
+
+    let mut rng = SmallRng::seed_from_u64(tick_start);
     for _ in 0..num_stars {
         let x = rng.gen_range(0..WIDTH - 2);
         let y = rng.gen_range(0..WIDTH - 2);
         graphics::win_fill_rectangle(layer_id, 4 + x, 24 + y, 2, 2, 0xfff100);
     }
+
+    let TimerInfo { tick: tick_end, .. } = time::get_current_tick();
+    println!(
+        "{} stars in {} ms.",
+        num_stars,
+        (tick_end - tick_start) * 1000 / timer_freq
+    );
+
     0
 }
 
