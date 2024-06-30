8696eda384fbe6952aba95600c25e54992dd1ad5
kei519 <masaki.keigo.q00@kyoto-u.jp>
2024-06-30 19:18:56 +0900

    app-lib: win_redraw システムコールの対応と flags を用いたウィンドウ描画関数の追加

--- mikan-os/apps/app-lib/app-lib/src/graphics.rs
+++ mikan-os/apps/app-lib/app-lib/src/graphics.rs
@@ -5,6 +5,30 @@ use core::{
 
 use crate::{buf::CStrBuf, *};
 
+/// ウィンドウ描画時のフラグを表す。
+//  0 bit: 再描画を行わない。
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct LayerFlags(u32);
+
+impl LayerFlags {
+    pub const fn new() -> Self {
+        Self(0)
+    }
+
+    pub fn redraw_enable(&self) -> bool {
+        self.0 & 1 == 0
+    }
+
+    pub fn set_redraw(mut self, redraw: bool) -> Self {
+        if redraw {
+            self.0 &= !0b1;
+        } else {
+            self.0 |= 0b1;
+        }
+        self
+    }
+}
+
 /// 作成したウィンドウのレイヤー ID を返す。
 /// ただし作成に失敗した場合は `0` を返す。
 pub fn open_window(w: i32, h: i32, x: i32, y: i32, title: impl Display) -> u32 {
@@ -40,10 +64,73 @@ pub fn win_write_string(layer_id: u32, x: i32, y: i32, color: u32, s: impl Displ
     }
 }
 
+pub fn win_write_string_with_flags(
+    layer_id: u32,
+    x: i32,
+    y: i32,
+    color: u32,
+    s: impl Display,
+    flags: LayerFlags,
+) {
+    let mut buf = [0; 1024];
+    let mut buf = CStrBuf::new_unchecked(&mut buf);
+    write!(buf, "{}", s).unwrap();
+    let res = unsafe {
+        syscall::__win_write_string(
+            layer_id as u64 | (flags.0 as u64) << 32,
+            x as _,
+            y as _,
+            color as _,
+            buf.to_cstr().as_ptr() as _,
+        )
+    };
+    if res.error != 0 {
+        ERRNO.store(res.error, Relaxed);
+    }
+}
+
 pub fn win_fill_rectangle(layer_id: u32, x: i32, y: i32, w: i32, h: i32, color: u32) {
     let res = unsafe {
         syscall::__win_fill_rectangle(layer_id as _, x as _, y as _, w as _, h as _, color as _)
     };
+    if res.error != 0 {
+        ERRNO.store(res.error, Relaxed);
+    }
+}
+
+pub fn win_fill_rectangle_with_flags(
+    layer_id: u32,
+    x: i32,
+    y: i32,
+    w: i32,
+    h: i32,
+    color: u32,
+    flags: LayerFlags,
+) {
+    let res = unsafe {
+        syscall::__win_fill_rectangle(
+            layer_id as u64 | (flags.0 as u64) << 32,
+            x as _,
+            y as _,
+            w as _,
+            h as _,
+            color as _,
+        )
+    };
+    if res.error != 0 {
+        ERRNO.store(res.error, Relaxed);
+    }
+}
+
+pub fn win_redraw(layer_id: u32) {
+    let res = unsafe { syscall::__win_redraw(layer_id as _) };
+    if res.error != 0 {
+        ERRNO.store(res.error, Relaxed);
+    }
+}
+
+pub fn win_redraw_with_flags(layer_id: u32, flags: LayerFlags) {
+    let res = unsafe { syscall::__win_redraw(layer_id as u64 | (flags.0 as u64) << 32) };
     if res.error != 0 {
         ERRNO.store(res.error, Relaxed);
     }
--- mikan-os/apps/app-lib/app-lib/src/syscall.rs
+++ mikan-os/apps/app-lib/app-lib/src/syscall.rs
@@ -38,9 +38,27 @@ syscall!(log_string, 0x8000_0000, log_level, s);
 syscall!(put_string, 0x8000_0001, fd, buf, len);
 syscall!(exit, 0x8000_0002, code);
 syscall!(open_window, 0x8000_0003, w, h, x, y, title);
-syscall!(win_write_string, 0x8000_0004, layer_id, x, y, color, s);
-syscall!(win_fill_rectangle, 0x8000_0005, layer_id, x, y, w, h, color);
+syscall!(
+    win_write_string,
+    0x8000_0004,
+    layer_id_flags,
+    x,
+    y,
+    color,
+    s
+);
+syscall!(
+    win_fill_rectangle,
+    0x8000_0005,
+    layer_id_flags,
+    x,
+    y,
+    w,
+    h,
+    color
+);
 syscall!(get_current_tick, 0x8000_0006);
+syscall!(win_redraw, 0x8000_0007, layer_id_flags);
 
 #[repr(C)]
 #[derive(Debug, Default, Clone, Copy)]
